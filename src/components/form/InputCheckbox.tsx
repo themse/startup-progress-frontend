34fe283ab4a69228dc287537122af9f5324ab3d1
Den Polusmyak <den.polusmyak@gmail.com>
2023-01-20 06:56:07 +0200

    implement checkbox disabling

--- src/components/form/InputCheckbox.tsx
+++ src/components/form/InputCheckbox.tsx
@@ -3,16 +3,28 @@ import clsx from 'clsx';
 
 import { Checkmark as CheckmarkIcon } from 'components/icons/Checkmark';
 
-const Checkbox: FC<{ isChecked?: boolean }> = ({ isChecked = false }) => {
+const Checkbox: FC<{ isChecked?: boolean; isDisabled?: boolean }> = ({
+  isChecked = false,
+  isDisabled = false,
+}) => {
   return (
     <div
       className={clsx([
         'flex justify-center items-center rounded-sm w-3.5 h-3.5',
-        isChecked && 'bg-primary',
+        isChecked && !isDisabled && 'bg-primary',
+        isDisabled && 'bg-secondary',
         !isChecked && 'border border-secondary',
       ])}
     >
-      {isChecked && <CheckmarkIcon className="w-2 h-2 text-white" />}
+      {isChecked && (
+        <CheckmarkIcon
+          className={clsx([
+            'w-2 h-2',
+            !isDisabled && 'text-white',
+            isDisabled && 'text-secondary',
+          ])}
+        />
+      )}
     </div>
   );
 };
@@ -23,6 +35,7 @@ type Props = {
 
   label?: string;
   isChecked?: boolean;
+  isDisabled?: boolean;
 };
 
 export const InputCheckbox: FC<Props> = ({
@@ -31,21 +44,28 @@ export const InputCheckbox: FC<Props> = ({
   onChange: onChangeProp,
 
   isChecked = false,
+  isDisabled = false,
 }) => {
   const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
     onChangeProp({ name, isChecked: event.target.checked });
   };
 
   return (
-    <label className="cursor-pointer flex items-center gap-2 text-sm">
+    <label
+      className={clsx([
+        'flex items-center gap-2 text-sm',
+        !isDisabled && 'cursor-pointer',
+      ])}
+    >
       <input
         name={name}
         type="checkbox"
         checked={isChecked}
-        className="hidden"
+        className={clsx(['hidden', isDisabled && 'pointer-events-none'])}
         onChange={onChange}
+        disabled={isDisabled}
       />
-      <Checkbox isChecked={isChecked} />
+      <Checkbox isChecked={isChecked} isDisabled={isDisabled} />
       {label && <p>{label}</p>}
     </label>
   );
